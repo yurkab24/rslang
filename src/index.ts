d4d8b75b79510710a5655d155d2c580154dc28b0
tyzikova <tyzikova27@mail.ru>
2022-02-04 11:03:40 +0300

    fix: correct files (Fri, Feb 04, 2022 5r)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import './index.scss';
+import './style';
 import App from './pages/app/app';
+import { addElementWrapper, addElementSection, addButtonsOfSection } from './views';
 
 const app = new App();
 app.run();
+
+addElementWrapper();
+addElementSection();
+addButtonsOfSection();
