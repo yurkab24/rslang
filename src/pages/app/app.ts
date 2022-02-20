30125d2d11ce8877f69293124b2cde4daf954be5
TatsiaA <taniokk@mail.ru>
2022-02-21 00:38:58 +0300

    fix: remove footer from games pages, url from main page, lint errors

--- src/pages/app/app.ts
+++ src/pages/app/app.ts
@@ -86,12 +86,13 @@ class App {
     this.enableRouteChange();
     App.container.append(this.footer.render());
 
+    if (location.hash) history.replaceState({}, document.title, location.href.split('#')[0]);
     window.addEventListener('hashchange', () => {
       if (window.location.hash.slice(1) === 'game-challenge' || window.location.hash.slice(1) === 'game-sprint') {
-      document.querySelector('.footer')?.classList.add('hidden');
-    } else {
-      document.querySelector('.footer')?.classList.remove('hidden');
-    }
+        document.querySelector('.footer')?.classList.add('hidden');
+      } else {
+        document.querySelector('.footer')?.classList.remove('hidden');
+      }
     });
   }
 }
